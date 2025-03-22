069f8d19fea28f5fc00644571abab7efae42ef65
MoldabayevaMadina <98735146+MoldabayevaMadina@users.noreply.github.com>
2025-03-22 17:22:29 +0500

    added lab8

--- lab8/shop_back/api/urls.py
+++ lab8/shop_back/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from .views import product_list, product_detail, category_list, category_detail, category_products
 from .views import products, product_by_id, categories, category_by_id, products_by_category
 
 urlpatterns = [
